ff48b708cf311557db456fb604315e46e9720f6c
Malebatso <155960470+Malebatso@users.noreply.github.com>
2026-05-03 21:24:38 +0200

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { decreaseQuantity, increaseQuantity, removeFromCart } from '../redux/CartSlice';
 
 const CartItem = () => {
   const dispatch = useDispatch();
+  const [checkoutMessage, setCheckoutMessage] = useState('');
   const cartItems = Object.values(useSelector((state) => state.cart.items));
 
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
@@ -16,11 +18,15 @@ const CartItem = () => {
       <p>Total Cost: ${totalCost.toFixed(2)}</p>
       <div className="cart-actions">
         <Link to="/plants" className="btn">Continue Shopping</Link>
-        <button className="btn primary" onClick={() => alert('Coming Soon!')}>Checkout</button>
+        <button className="btn primary" onClick={() => setCheckoutMessage('Checkout is coming soon!')}>
+          Checkout
+        </button>
       </div>
+      {checkoutMessage && <p className="checkout-msg">{checkoutMessage}</p>}
+      {cartItems.length === 0 && <p>Your cart is empty. Add some plants to get started.</p>}
       {cartItems.map((item) => (
         <article key={item.id} className="cart-card">
-          <img src={item.image} alt={item.name} />
+          <img src={item.image} alt={item.name} loading="lazy" />
           <div>
             <h4>{item.name}</h4>
             <p>Unit Price: ${item.price.toFixed(2)}</p>
