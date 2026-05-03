4a86d0504710c1d7f7e246b4973de34b923bd20a
Malebatso <155960470+Malebatso@users.noreply.github.com>
2026-05-03 21:50:53 +0200

    Refactor App component structure and remove location

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AboutUs from './components/AboutUs';
 import ProductList from './components/ProductList';
 import CartItem from './components/CartItem';
 import Header from './components/Header';
 
 const App = () => {
-  const location = useLocation();
-  const showHeader = location.pathname !== '/';
-
   return (
-    <div className={location.pathname === '/' ? 'landing-page' : 'app-shell'}>
-      {showHeader && <Header />}
-      <Routes>
-        <Route path="/" element={<AboutUs />} />
-        <Route path="/plants" element={<ProductList />} />
-        <Route path="/cart" element={<CartItem />} />
-      </Routes>
+    <div className="app-shell">
+      <Header />
+      <main className="background-image">
+        <Routes>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/plants" element={<ProductList />} />
+          <Route path="/cart" element={<CartItem />} />
+        </Routes>
+      </main>
     </div>
   );
 };
